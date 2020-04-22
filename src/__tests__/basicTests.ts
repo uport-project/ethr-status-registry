130185ea280de00c585124080b225f3942148cbb
Mircea Nistor <mirceanis@gmail.com>
2020-04-22 12:53:41 +0200

    fix(test): fix rpc url for tests

--- src/__tests__/basicTests.ts
+++ src/__tests__/basicTests.ts
@@ -23,6 +23,9 @@ const referenceDoc = {
                        ]
                      } as DIDDocument
 
+//only usable for this test
+const rinkebyRPC = 'https://rinkeby.infura.io/v3/ec9c99d75b834bac8dd4bfacad8cfdf7'
+
 it('should be able to instantiate Status using infura ID', () => {
   expect(new EthrStatusRegistry({ infuraProjectId: 'none' })).not.toBeNil()
 })
@@ -74,7 +77,7 @@ it(`should return valid credential status`, async () => {
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzI5NzM2MjMsInN0YXR1cyI6eyJ0eXBlIjoiRXRoclN0YXR1c1JlZ2lzdHJ5MjAxOSIsImlkIjoicmlua2VieToweDFFNDY1MWRjYTVFZjM4NjM2ZTJFNEQ3QTZGZjRkMjQxM2ZDNTY0NTAifSwiaXNzIjoiZGlkOmV0aHI6MHhmM2JlYWMzMGM0OThkOWUyNjg2NWYzNGZjYWE1N2RiYjkzNWIwZDc0In0.CFDlVKGWBiJwUwq14waLQ2fqLljhJG3Qci5KFhcF8zM916sN7MWFESdF1TseIOPmIcteQ_99m61dTTJ0YMY0rwE'
   const statusChecker = new EthrStatusRegistry({
     networks: [
-      { name: 'rinkeby', rpcUrl: 'https://rinkeby.infura.io/ethr-did' }
+      { name: 'rinkeby', rpcUrl: rinkebyRPC }
     ]
   })
   await expect(statusChecker.checkStatus(token, referenceDoc)).resolves.toMatchObject({
@@ -87,7 +90,7 @@ it(`should return revoked credential status`, async () => {
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NzMwNDczNTEsInN0YXR1cyI6eyJ0eXBlIjoiRXRoclN0YXR1c1JlZ2lzdHJ5MjAxOSIsImlkIjoicmlua2VieToweDFFNDY1MWRjYTVFZjM4NjM2ZTJFNEQ3QTZGZjRkMjQxM2ZDNTY0NTAifSwiaXNzIjoiZGlkOmV0aHI6MHgxZmNmOGZmNzhhYzUxMTdkOWM5OWI4MzBjNzRiNjY2OGQ2YWMzMjI5In0.MHabafA0UxJuQJ0Z-7Egb57WRlgj4_zf96B0LUhRyXgVDU5RABIczTTTXWjcuKVzhJc_-FuhRI8uQYmQQNxKzgA'
   const statusChecker = new EthrStatusRegistry({
     networks: [
-      { name: 'rinkeby', rpcUrl: 'https://rinkeby.infura.io/ethr-did' }
+      { name: 'rinkeby', rpcUrl: rinkebyRPC }
     ]
   })
 
