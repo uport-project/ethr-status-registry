e14956a3f986d6e2225b12a52ba47fe032e737e8
Mircea Nistor <mirceanis@gmail.com>
2020-05-08 22:34:36 +0200

    feat: implement address recovery in ExternalSignerProvider

--- src/ExternalSignerProvider.ts
+++ src/ExternalSignerProvider.ts
@@ -11,35 +11,43 @@ export class ExternalSignerProvider extends ethers.Signer {
     ethers.utils.defineReadOnly(this, 'provider', provider)
   }
 
-  sign(transaction: ethers.providers.TransactionRequest): Promise<string> {
-    return ethers.utils.resolveProperties(transaction).then((tx) => {
-      return new Promise((resolve, reject) => {
-        this.ethSign(tx, (err, signedTx) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(signedTx)
-          }
-        })
-      })
-    })
+  async sign(transaction: ethers.providers.TransactionRequest): Promise<string> {
+    const tx = await ethers.utils.resolveProperties(transaction)
+    const signed = await this.signRaw(tx)
+    return signed
   }
 
-  getAddress(): Promise<string> {
-    throw new Error('Method not implemented.')
+  async getAddress(): Promise<string> {
+    const signedTx = await this.signRaw({})
+    const decoded = ethers.utils.parseTransaction(signedTx)
+    return decoded.from
   }
 
   signMessage(message: ethers.utils.Arrayish): Promise<string> {
-    throw new Error('Method not implemented.')
+    return Promise.reject(new Error('Method not implemented.'))
   }
 
-  sendTransaction(transaction: ethers.providers.TransactionRequest): Promise<ethers.providers.TransactionResponse> {
+  async sendTransaction(
+    transaction: ethers.providers.TransactionRequest
+  ): Promise<ethers.providers.TransactionResponse> {
     if (!this.provider) {
       throw new Error('missing provider')
     }
 
     return this.sign(transaction).then((signedTransaction) => {
       return this.provider.sendTransaction(signedTransaction)
+    })
+  }
+
+  private signRaw = (rawTx: any): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      this.ethSign(rawTx, (err: any, signedTx: string) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(signedTx)
+        }
+      })
     })
   }
 }
